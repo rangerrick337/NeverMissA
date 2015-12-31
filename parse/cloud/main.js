d9a5165122dab50c4a59e72a2076eb0242100561
rangerrick337 <JustBe@Lees-MacBook-Pro.local>
2015-12-30 23:03:09 -0600

    Now partially pushing iTunes album data into Parse
    Artist, Album Name, and Price correctly populating in Parse Class Album

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,29 +1,45 @@
-var xmlreader = require('cloud/modules/xmlreader.js')
+var xmlreader = require('cloud/modules/xmlreader.js');
 
 Parse.Cloud.job("GetAlbums", function(request, response) {
 
   Parse.Cloud.httpRequest({
     url: 'https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/justadded/sf=143441/limit=100/explicit=true/rss.xml'
   }).then(function(httpResponse) {
-      console.log("We made it say this");
       xmlreader.read(httpResponse.buffer.toString(), function(err, res) {
         if(err) return console.log(err);
-        res.rss.channel.item.each(function(i, item){
-          Parse.Cloud.run('addAlbumInfoToAlbums', item['itms:artist'].text() );
-		    console.log( item['itms:artist'].text() );
-        });
-        response.success('xmlreader ran');
-      });
-      // response.success("ended on httprequest success")
-    }, function(httpResponse) {
-      response.error('Request failed with response code ' + httpResponse.status);
-    });
-  });
 
-Parse.Cloud.define('addAlbumInfoToAlbums', function(request, response) {
-  Parse.addUnique()
+        var toSaves = [];
+        res.rss.channel.item.each(function(i, item){
+          var AlbumToAdd = Parse.Object.extend("Album");
+          var albumToAdd = new AlbumToAdd();
+
+          // var price = item['itms:albumPrice'];
+          // var price_as_number = price.replace(/\$|,/g, '');
+          albumToAdd.set("artist_name", item['itms:artist'].text());
+          albumToAdd.set("album_name", item['itms:album'].text());
+          albumToAdd.set("album_price", item['itms:albumPrice'].text());
+          console.log(albumToAdd);
+
+          return toSaves.push(albumToAdd);
+        });
+        Parse.Object.saveAll(toSaves, {
+          success: function(savelist) {
+            response.success('albums should be added to Album object in Parse')
+          }, error: function(error) {
+          response.error('Request failed with response code ' + error.status);
+          }
+        });
+      });
+    });
 });
 
+
+
+// }).then(function() {
+//       response.success('albums should be added to Album object in Parse')
+//   }), function(httpResponse) {
+//   response.error('Request failed with response code ' + httpResponse.status);
+// };
 
 
 // Previous Code:

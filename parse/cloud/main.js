6102f0257f0b2cae74ce7bcff4acd33b9804b9e7
rangerrick337 <JustBe@Lees-MacBook-Pro.local>
2016-01-01 20:36:25 -0600

    Filling everything in Parse except some rows have missing artwork

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -13,17 +13,22 @@ Parse.Cloud.job("GetAlbums", function(request, response) {
           var AlbumToAdd = Parse.Object.extend("Album");
           var albumToAdd = new AlbumToAdd();
           var date = new Date (item['itms:releasedate'].text());
-
-          // var price = item['itms:albumPrice'];
-          // var price_as_number = price.replace(/\$|,/g, '');
+          var url = item['itms:coverArt'].at(2).text();
           albumToAdd.set("artist_name", item['itms:artist'].text());
           albumToAdd.set("album_name", item['itms:album'].text());
           albumToAdd.set("album_price", item['itms:albumPrice'].text());
           albumToAdd.set("release_date", date);
           console.log(albumToAdd);
 
-          return toSaves.push(albumToAdd);
-        });
+          Parse.Cloud.httpRequest({url: url}).then(function(httpResponse) {
+            var albumImage = new Parse.File("albumImage", {base64: httpResponse.buffer.toString('base64')});
+            console.log(albumImage);
+            albumImage.save().then(function() {
+                albumToAdd.set("album_artwork", albumImage);
+            });
+          });
+          toSaves.push(albumToAdd);
+          });
         Parse.Object.saveAll(toSaves, {
           success: function(savelist) {
             response.success('albums should be added to Album object in Parse')
@@ -37,53 +42,8 @@ Parse.Cloud.job("GetAlbums", function(request, response) {
 
 
 
-// }).then(function() {
-//       response.success('albums should be added to Album object in Parse')
-//   }), function(httpResponse) {
-//   response.error('Request failed with response code ' + httpResponse.status);
-// };
-
-
-// Previous Code:
-// Parse.Cloud.job("runGetAlbums", function(request, response) {
-//
-//   Parse.Cloud.define("getAlbums", function(request, response) {
-//
-//     var xml = Parse.Cloud.HTTPResponse({
-//       url: "https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/justadded/sf=143441/limit=100/explicit=true/rss.xml",
-//       success: function(httpResponse) {
-//         console.log('xml fetched');
-//         response.success('xml fetched');
-//       },
-//       error: function(httpResponse) {
-//         console.error('xml failed to fetch');
-//         response.error('xml failed to fetch');
-//       },
-//     });
-//
-//     xmlreader.read(xml, function(err, res) {
-//       if (err) return console.error(err);
-//       console.log(res.channel.text() );
-//       response.success('xmlreader ran');
-//     });
-//   });
-//   response.success(Parse.Cloud.getAlbums());
-// });
-
-
-
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
-
-// Parse.Cloud.httpRequest({
-//   url: 'https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/justadded/sf=143441/limit=100/explicit=true/rss.xml'
-// }).then(function(httpResponse) {
-//   // success
-//   console.log(httpResponse.text);
-// },function(httpResponse) {
-//   // error
-//   console.error('Request failed with response code ' + httpResponse.status);
-// });

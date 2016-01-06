64ef22d704ed797a84a349a20252e5fb422a7ea4
rangerrick337 <JustBe@Lees-MacBook-Pro.local>
2016-01-06 01:59:56 -0600

    cleaned up

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,49 +1,60 @@
-var xmlreader = require('cloud/modules/xmlreader.js');
-
 Parse.Cloud.job("GetAlbums", function(request, response) {
+  var xmlReader = require('cloud/modules/xmlreader.js');
+  var itunesURL = 'https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/justadded/sf=143441/limit=100/explicit=true/rss.xml';
+
+  function readResponse_async(xlmString) {
+    var promise = new Parse.Promise();
+    xmlReader.read(xlmString, function (err, res) {
+        if(err) {
+            promise.reject(err);
+        } else {
+            promise.resolve(res);
+        }
+    });
+    return promise;
+  };
+
+  function getAlbums_async(res) {
+    var albums = [];
+    var promiseAlbum = new Parse.Promise();
+    res.rss.channel.item.each(function(i, item){
+      var promise = new Parse.Promise();
+      var AlbumToAdd = Parse.Object.extend("Album");
+      var albumToAdd = new AlbumToAdd();
+      var date = new Date (item['itms:releasedate'].text());
+      var url = item['itms:coverArt'].at(2).text();
+
+      albumToAdd.set("artist_name", item['itms:artist'].text());
+      albumToAdd.set("album_name", item['itms:album'].text());
+      albumToAdd.set("album_price", item['itms:albumPrice'].text());
+      albumToAdd.set("release_date", date);
+
+      Parse.Cloud.httpRequest({url: url}).then(function(httpResponse) {
+        var albumImage = new Parse.File("albumImage", {base64: httpResponse.buffer.toString('base64')});
+        albumImage.save().then(function() {
+          albumToAdd.set("album_artwork", albumImage);
+          promise.resolve(albumToAdd);
+        });
+      });
+      albums.push(promise);
+    });
+    console.log(albums);
+    return Parse.Promise.when(albums);
+  };
 
   Parse.Cloud.httpRequest({
-    url: 'https://itunes.apple.com/WebObjects/MZStore.woa/wpa/MRSS/justadded/sf=143441/limit=100/explicit=true/rss.xml'
+    url: itunesURL
   }).then(function(httpResponse) {
-      xmlreader.read(httpResponse.buffer.toString(), function(err, res) {
-        if(err) return console.log(err);
-
-        var toSaves = [];
-        res.rss.channel.item.each(function(i, item){
-          var AlbumToAdd = Parse.Object.extend("Album");
-          var albumToAdd = new AlbumToAdd();
-          var date = new Date (item['itms:releasedate'].text());
-          var url = item['itms:coverArt'].at(2).text();
-          albumToAdd.set("artist_name", item['itms:artist'].text());
-          albumToAdd.set("album_name", item['itms:album'].text());
-          albumToAdd.set("album_price", item['itms:albumPrice'].text());
-          albumToAdd.set("release_date", date);
-          console.log(albumToAdd);
-
-          Parse.Cloud.httpRequest({url: url}).then(function(httpResponse) {
-            var albumImage = new Parse.File("albumImage", {base64: httpResponse.buffer.toString('base64')});
-            console.log(albumImage);
-            albumImage.save().then(function() {
-                albumToAdd.set("album_artwork", albumImage);
-            });
-          });
-          toSaves.push(albumToAdd);
-          });
-        Parse.Object.saveAll(toSaves, {
+    return readResponse_async(httpResponse.buffer.toString())
+  }).then(function(res) {
+      return getAlbums_async(res)
+    }).then(function(albums) {
+        Parse.Object.saveAll(albums, {
           success: function(savelist) {
             response.success('albums should be added to Album object in Parse')
           }, error: function(error) {
-          response.error('Request failed with response code ' + error.status);
+            response.error('Request failed with response code ' + error.status);
           }
         });
       });
-    });
-});
-
-
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
 });
